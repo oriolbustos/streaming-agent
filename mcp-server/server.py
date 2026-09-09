338567acc4f912fe9a99127e01096a69214573a7
oriolbustos <167873201+oriol-bustos@users.noreply.github.com>
2026-09-09 18:15:29 +0200

    feat: new 3 tools for chinook
    Signed-off-by: oriolbustos <167873201+oriol-bustos@users.noreply.github.com>

--- mcp-server/server.py
+++ mcp-server/server.py
@@ -38,5 +38,41 @@ def tracks_by_genre(genre: str) -> list[str]:
             results = cur.fetchall()
             return [result[0] for result in results]
 
+@mcp.tool()
+def top_20_artists() -> list[dict]:
+    """
+    Returns a list with the top 20 artists with more tracks.
+    """
+
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT artist.name AS artist_name, COUNT(*) AS track_count FROM artist JOIN album ON artist.artist_id = album.artist_id JOIN track ON track.album_id = album.album_id GROUP BY artist.name ORDER BY track_count DESC LIMIT 20;")
+            results = cur.fetchall()
+            return [{"artists": result[0], "tracks": result[1]} for result in results]
+
+@mcp.tool()
+def sales_by_country() -> list[dict]:
+    """
+    Returns a list with the total sales by country in descending order.
+    """
+
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT billing_country, SUM(total) as sales FROM invoice GROUP BY billing_country ORDER BY sales DESC;")
+            results = cur.fetchall()
+            return [{"country": result[0], "sales": result[1]} for result in results]
+
+@mcp.tool()
+def list_schema() -> list[dict]:
+    """
+    Returns a list with table and column names of the tables of the database.
+    """
+
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT table_name, column_name FROM information_schema.columns WHERE table_schema = 'public' ORDER BY table_name, ordinal_position LIMIT 10;")
+            results = cur.fetchall()
+            return [{"table": result[0], "column": result[1]} for result in results]
+        
 if __name__ == "__main__":
     mcp.run()
